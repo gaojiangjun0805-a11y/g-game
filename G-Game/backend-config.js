4a45222205db6c1ff22a7f2429921f9f6df2b440
gaojiangjun0805-a11y <gaojiangjun0805@gmail.com>
2026-06-21 06:24:03 +0800

    Update game release

--- G-Game/backend-config.js
+++ G-Game/backend-config.js
@@ -1,5 +1,5 @@
 window.GGAME_BACKEND_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://ckhdhsgovvsjocmsqaqw.supabase.co',
+  supabaseAnonKey: 'sb_publishable_8Sr7uzDckWRW2aHDbPCcoA_KlL-vmx9',
   table: 'ggame_player_states'
 };
